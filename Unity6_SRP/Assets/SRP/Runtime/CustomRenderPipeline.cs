3494ce78881e02df3e6548447d4288145dde9af4
djl <timotimosky@users.noreply.github.com>
2024-07-16 21:02:45 +0800

    优化公共变量

--- Unity6_SRP/Assets/SRP/Runtime/CustomRenderPipeline.cs
+++ Unity6_SRP/Assets/SRP/Runtime/CustomRenderPipeline.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Rendering.RendererUtils;
 using Conditional = System.Diagnostics.ConditionalAttribute;
 
 //这里也显示出了SRP的第一个好处，虽然都是前向渲染，但DrawCall明显变少了。非SRP虽然也可以单Pass多光源，但除平行光外，
@@ -19,7 +22,7 @@ namespace Tiny_RenderPipeline
         //Unity的延迟执行体现在CommandBuffer和ScriptableRenderContext的设计中，这两个对象都充当我们的“菜单”。
         //将需要执行的执行记录在菜单上以后，
         //可以使用ScriptableRenderContext.ExecuteCommandBuffer和ScriptableRenderContext.Submit来提交CommandBuffer和ScriptableRenderContext。
-        CommandBuffer myCommandBuffer;
+        public CommandBuffer myCommandBuffer;
 
         //在不继续渲染的时候（例如我们切换到了另一个渲染管线）对当前管线进行现场清理。
         protected override void Dispose(bool dis)
@@ -101,75 +104,83 @@ namespace Tiny_RenderPipeline
         Vector4[] PLightColors = new Vector4[maxPointLights];
         Vector4[] PLightPos = new Vector4[maxPointLights];
 
-
-
-
-
         void DrawVisibleGeometry()
         {
           // context.DrawSkybox(camera);
         }
 
-        //Render函数用于每一帧执行所有的渲染，
-        //Render函数接受两个参数：第一个是被称为ScriptableRenderContext的新概念，我们会在后面介绍，
-        //第二个是一个相机数组，包含了所有需要渲染的相机列表。我们一般需要针对列表里每一个相机运行一次管线，
-        //但是针对相机种类的不同，可能会使用不同的渲染流程。
-        void Render_01_sky_box(Camera camera, ScriptableRenderContext renderContext, int D_LightDir, int D_LightColor, int _CameraPos, int _PLightPos, int _PLightColor)
+        static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+        static ShaderTagId BaseLitShaderTagId = new ShaderTagId("BaseLit");
+
+        //剔除：拿到场景中的所有渲染器，然后剔除那些在摄像机视锥范围之外的渲染器。
+        bool Cull(Camera camera, ScriptableRenderContext renderContext,out CullingResults cullResults)
         {
-            //将上下文设置为当前相机的上下文。
-            renderContext.SetupCameraProperties(camera);
-            //设置渲染目标的颜色为蓝色。
-            myCommandBuffer.ClearRenderTarget(true, true, Color.blue);
-            //提交指令队列至当前context处理。
-            renderContext.ExecuteCommandBuffer(myCommandBuffer);
-            //清空当前指令队列。
-            myCommandBuffer.Clear();
-            //开始执行上下文
-            renderContext.Submit();
+            //渲染器：它是附着在游戏对象上的组件，可将它们转变为可以渲染的东西。通常是一个MeshRenderer组件。
+
+            //首先判断相机是否支持渲染
+            if (camera.TryGetCullingParameters(out ScriptableCullingParameters cullParam))
+            {
+                cullParam.isOrthographic = false;
+
+                //refout用作优化，以防止传递结构的副本，该副本相当大。cullParam是一个结构而不是一个对象是另一种优化，以防止内存分配。
+                cullResults = renderContext.Cull(ref cullParam);
+                return true;
+            }
+            cullResults = new CullingResults();
+            return false;
+        }
+
+        //绘制可见的场景物体
+        void DrawVisibleGeometry(CullingResults cullResults,Camera camera, ScriptableRenderContext renderContext)
+        {
+            //过滤：决定使用哪些渲染器
+            FilteringSettings filtSet = new FilteringSettings(RenderQueueRange.opaque, -1);
+            //filtSet.renderQueueRange = RenderQueueRange.opaque;
+            //filtSet.layerMask = -1;                
+
+
+            //相机用于设置排序和剔除层，而DrawingSettings控制使用哪个着色器过程进行渲染。
+
+
+            //确定是应用正交排序还是基于距离的排序
+            //决定使用何种渲染排序顺序 对应shader里的	Tags{ "Queue" = "Geometry" } 这属性(不是这个单一属性)
+            //opaque涵盖了从0到2500（包括2500）之间的渲染队列。
+
+
+            // CommonOpaque排序时，大部分时候，对象从前到后绘制，这对于不透明的对象来说是理想的选择：
+            // 如果某物最终被绘制在其他物后面，则可以跳过其隐藏的片段，从而加快渲染速度。
+            // 常见的不透明排序选项还考虑了一些其他条件，包括渲染队列和材质。
+
+            SortingSettings sortSet = new SortingSettings(camera) { criteria = SortingCriteria.CommonOpaque };
+
+            //决定使用何种light mode，对应shader的pass的tag中的LightMode
+            DrawingSettings drawSet = new DrawingSettings(BaseLitShaderTagId, sortSet);
+
+
+            //1.绘制不透明物体
+            renderContext.DrawRenderers(cullResults, ref drawSet, ref filtSet);
+
+            //2.绘制天空球,在不透明物体之后绘制。early-z避免不必要的overdraw。
+            renderContext.DrawSkybox(camera);
+
+
+            //3.绘制透明物体
+            //，RenderQueueRange.transparent在渲染天空盒之后，将队列范围更改为从2501到5000，包括5000，然后再次渲染。
+            filtSet.renderQueueRange = RenderQueueRange.transparent;
+            sortSet.criteria = SortingCriteria.CommonTransparent;
+
+
+            //必须指出允许哪种着色器通道
+            //由于我们仅在管道中支持未照明的材质，因此我们将使用Unity的默认未照明通道，该通道由SRPDefaultUnlit标识。
+           // drawSet = new DrawingSettings(unlitShaderTagId, sortSet);
+
+           // renderContext.DrawRenderers(cullResults, ref drawSet, ref filtSet);
         }
 
 
-        //完整的基本的兰伯特光照脚本
-        void Render_mutil_light(Camera camera, ScriptableRenderContext renderContext,int D_LightDir,int D_LightColor,int _CameraPos,int _PLightPos,int _PLightColor)
+
+        void InitLight(CullingResults cullResults, Camera camera, ScriptableRenderContext renderContext, int D_LightDir, int D_LightColor, int _CameraPos, int _PLightPos, int _PLightColor)
         {
-            myCommandBuffer.BeginSample("Render " + camera.name);
-
-            //设置渲染相关相机参数,包含相机的各个矩阵和剪裁平面等
-            renderContext.SetupCameraProperties(camera);
-
-
-            //清理myCommandBuffer，设置渲染目标的颜色为摄像机背景
-            var flags = camera.clearFlags;
-
-            //myCommandBuffer.ClearRenderTarget(true,true,camera.backgroundColor);
-
-            myCommandBuffer.ClearRenderTarget((flags & CameraClearFlags.Depth) != 0, (flags & CameraClearFlags.Color) != 0, camera.backgroundColor);
-
-#if UNITY_EDITOR
-            //为了在场景视图中看到UI
-            //尽管Unity帮我们适配了UI在游戏窗口中显示，但不会在场景窗口显示。
-            //UI始终存在于场景窗口中的世界空间中，但是我们必须手动将其注入场景中。
-            //通过调用static ScriptableRenderContext.EmitWorldGeometryForSceneView方法（以当前摄像机为参数）来添加UI 。
-            //必须在cull之前完成此操作。
-            //为了避免游戏窗口中第二次添加UI。我们仅在渲染场景窗口时才发出UI几何。
-            //cameraType相机的等于时就是这种情况CameraType.SceneView。
-            if (camera.cameraType == CameraType.SceneView)
-            {
-                ScriptableRenderContext.EmitWorldGeometryForSceneView(camera);
-            }
-#endif
-
-            //剔除：拿到场景中的所有渲染器，然后剔除那些在摄像机视锥范围之外的渲染器。
-
-            //渲染器：它是附着在游戏对象上的组件，可将它们转变为可以渲染的东西。通常是一个MeshRenderer组件。
-            ScriptableCullingParameters cullParam = new ScriptableCullingParameters();
-            camera.TryGetCullingParameters(out cullParam);
-            cullParam.isOrthographic = false;
-
-            //TODO：避免没有任何可渲染的物体时，往下渲染
-            CullingResults cullResults = renderContext.Cull(ref cullParam);
-            renderContext.DrawSkybox(camera);
-
             //在剪裁结果中获取灯光并进行参数获取
             var lights = cullResults.visibleLights;
 
@@ -225,44 +236,94 @@ namespace Tiny_RenderPipeline
 
             myCommandBuffer.SetGlobalVectorArray(_PLightColor, PLightColors);
             myCommandBuffer.SetGlobalVectorArray(_PLightPos, PLightPos);
-            myCommandBuffer.EndSample("Render " + camera.name);
-            //执行CommandBuffer中的指令
+        }
+
+        public string sampleName = "Render camera";
+        void Setup(Camera camera, ScriptableRenderContext renderContext)
+        {
+            sampleName = "Render camera =>" + camera.name;
+            myCommandBuffer.BeginSample(sampleName);
+
+            //如果不在最前面设置相机，那么后续的ClearRenderTarget将使用draw GL（执行一次空绘制）来清理，比较低效，需要耗费一次set passCall
+            //在最前面设置相机参数后，，则可以使用直接 clear(depth + stencil)，清理camera的深度 +模板
+            //设置渲染相关相机参数,包含相机的各个矩阵和剪裁平面等
+            renderContext.SetupCameraProperties(camera);
+
+            //清除渲染目标
+            //无论我们绘制什么，最终都会渲染到相机的渲染目标，默认情况下是帧缓冲区，但也可能是渲染纹理。上一帧的缓冲区或者纹理，不会自动失效
+            //之前被绘制到该目标的任何内容仍然存在，这可能会干扰我们现在渲染的图像。
+            //为了保证正确的渲染，我们必须清除渲染目标以摆脱其旧内容。
+            //CommandBuffer.ClearRenderTarget至少需要三个参数。前两个指示是否应清除深度和颜色数据，这两者都是如此。第三个参数是用于清除的颜色
+            //2.设置渲染目标：是否保留深度、背景色。
+            var flags = camera.clearFlags;
+            myCommandBuffer.ClearRenderTarget((flags & CameraClearFlags.Depth) != 0, (flags & CameraClearFlags.Color) != 0, camera.backgroundColor);
+
+
+            //每一次 myCommandBuffer填充了，必须执行，才有效，不是光执行ClearRenderTarget就可以了
+            ExecuteBuffer(camera, renderContext);
+
+        }
+
+        void Submit(Camera camera, ScriptableRenderContext renderContext)
+        {           
+            myCommandBuffer.EndSample(sampleName);
+            //6.渲染上下文提交gpu
+            renderContext.Submit();
+        }
+
+        //每个setpasscall，都要调用一次
+        void ExecuteBuffer(Camera camera, ScriptableRenderContext renderContext)
+        {
+            //3.复制CommandBuffer指令，填充到上下文
             renderContext.ExecuteCommandBuffer(myCommandBuffer);
-
+            //CommandBuffer指令可以重用，所以我们手动清理下
             myCommandBuffer.Clear();
+        }
 
-            //过滤：决定使用哪些渲染器
-            FilteringSettings filtSet = new FilteringSettings(RenderQueueRange.opaque, -1);
-            //filtSet.renderQueueRange = RenderQueueRange.opaque;
-            //filtSet.layerMask = -1;                
+        //完整的基本的兰伯特光照脚本
+        void Render_mutil_light(Camera camera, ScriptableRenderContext renderContext,int D_LightDir,int D_LightColor,int _CameraPos,int _PLightPos,int _PLightColor)
+        {
 
-
-            //相机用于设置排序和剔除层，而DrawingSettings控制使用哪个着色器过程进行渲染。
-
-            //决定使用何种渲染排序顺序 对应shader里的	Tags{ "Queue" = "Geometry" } 这属性(不是这个单一属性)
-            //opaque涵盖了从0到2500（包括2500）之间的渲染队列。
-            SortingSettings sortSet = new SortingSettings(camera) { criteria = SortingCriteria.CommonOpaque };
-            //决定使用何种light mode，对应shader的pass的tag中的LightMode
-            DrawingSettings drawSet = new DrawingSettings(new ShaderTagId("BaseLit"), sortSet);
+            //避免没有任何可渲染的物体时，往下渲染
+            if (!Cull(camera, renderContext, out CullingResults cullResults))
+            {
+                Debug.Log("没有可渲染物体");
+                return;
+            }
 
 
-            //1.绘制不透明物体
-            renderContext.DrawRenderers(cullResults, ref drawSet, ref filtSet);
+            Setup(camera, renderContext);
 
-            //2.绘制天空球,在不透明物体之后绘制。early-z避免不必要的overdraw。
+#if UNITY_EDITOR
+            //为了在场景视图中看到UI
+            //尽管Unity帮我们适配了UI在游戏窗口中显示，但不会在场景窗口显示。
+            //UI始终存在于场景窗口中的世界空间中，但是我们必须手动将其注入场景中。
+            //通过调用static ScriptableRenderContext.EmitWorldGeometryForSceneView方法（以当前摄像机为参数）来添加UI 。
+            //必须在cull之前完成此操作。
+            //为了避免游戏窗口中第二次添加UI。我们仅在渲染场景窗口时才发出UI几何。
+            //cameraType相机等于CameraType.SceneView的时候就是这种情况。
+            if (camera.cameraType == CameraType.SceneView)
+            {
+                ScriptableRenderContext.EmitWorldGeometryForSceneView(camera);
+            }
+#endif
+            //初始化一个RendererList
+            // RendererListDesc desc = new RendererListDesc();
+            // RendererList rendererList = renderContext.CreateRendererList(desc);
+
+            //rendererList.add
+
             renderContext.DrawSkybox(camera);
 
+            //2.初始化灯光信息到CommandBuffer指令
+
+            InitLight(cullResults, camera, renderContext, D_LightDir, D_LightColor, _CameraPos, _PLightPos, _PLightColor);
 
 
+            ExecuteBuffer(camera, renderContext);
 
-            //3.绘制透明物体
-            //，RenderQueueRange.transparent在渲染天空盒之后，将队列范围更改为从2501到5000，包括5000，然后再次渲染。
-            filtSet.renderQueueRange = RenderQueueRange.transparent;
-            sortSet.criteria = SortingCriteria.CommonTransparent;
-            //由于我们仅在管道中支持未照明的材质，因此我们将使用Unity的默认未照明通道，该通道由SRPDefaultUnlit标识。
-            drawSet = new DrawingSettings(new ShaderTagId("SRPDefaultUnlit"), sortSet);
-            renderContext.DrawRenderers(cullResults, ref drawSet, ref filtSet);
-
+            //5.渲染物体的指令填充到上下文
+            DrawVisibleGeometry (cullResults,camera,  renderContext);
             //绘制错误
             //由于我们的管道仅支持未着色的着色器，因此不会渲染使用不同着色器的对象，从而使它们不可见。尽管这是正确的，
             //但它掩盖了某些对象使用错误着色器的事实。如果我们使用Unity的错误着色器可视化这些对象，那将是很好的，
@@ -270,32 +331,37 @@ namespace Tiny_RenderPipeline
             //在绘制透明形状之后，我们将在最后调用它。
             DrawErrorShaderObject(cullResults, renderContext, camera);
 
-
-            //开始执行渲染内容
-            renderContext.Submit();
+            //将这个renderlist加入myCommandBuffer
+            // myCommandBuffer.DrawRendererList(rendererList);
+            Submit(camera, renderContext);
         }
 
+        //将shader中需要的属性参数映射为ID，加速传参
+        int D_LightDir = Shader.PropertyToID("_DLightDir");
+        int D_LightColor = Shader.PropertyToID("_DLightColor");
+        //在设置灯光参数ID下面增加相机参数ID：
+        int _CameraPos = Shader.PropertyToID("_CameraPos");
+
+        int _PLightPos = Shader.PropertyToID("_PLightPos");
+        int _PLightColor = Shader.PropertyToID("_PLightColor");
+
+
+        //Render函数用于每一帧执行所有的渲染，
+        //Render函数接受两个参数：第一个是被称为ScriptableRenderContext的新概念，我们会在后面介绍，
+        //第二个是一个相机数组，包含了所有需要渲染的相机列表。我们一般需要针对列表里每一个相机运行一次管线，
+        //但是针对相机种类的不同，可能会使用不同的渲染流程。
         protected void RenderAllCamera(ScriptableRenderContext renderContext, List<Camera> cameras)
         {
 
             //渲染开始后，创建CommandBuffer;
             if (myCommandBuffer == null)
-                myCommandBuffer = new CommandBuffer() { name = "SRP Study CB" };
-
-            //将shader中需要的属性参数映射为ID，加速传参
-            var D_LightDir = Shader.PropertyToID("_DLightDir");
-            var D_LightColor = Shader.PropertyToID("_DLightColor");
-            //在设置灯光参数ID下面增加相机参数ID：
-            var _CameraPos = Shader.PropertyToID("_CameraPos");
-
-            var _PLightPos = Shader.PropertyToID("_PLightPos");
-            var _PLightColor = Shader.PropertyToID("_PLightColor");
+                myCommandBuffer = new CommandBuffer() { name = "First SRP Render CommandBuffer" };
 
             //所有相机开始逐次渲染
-            foreach (var camera in cameras)
+            for (int i = 0; i < cameras.Count; ++i) //不要使用foreach，以后要对摄像机排序
             {
+                var camera = cameras[i];
                 Render_mutil_light(camera, renderContext, D_LightDir, D_LightColor, _CameraPos, _PLightPos, _PLightColor);
-               // Render_01_sky_box(camera, renderContext, D_LightDir, D_LightColor, _CameraPos, _PLightPos, _PLightColor);
             }
         }
 
