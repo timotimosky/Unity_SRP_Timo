896486f621727d8799a587104482f80047c77346
djl <timotimosky@users.noreply.github.com>
2024-08-09 17:51:06 +0800

    测试实例化

---
+++ Unity6_SRP/Assets/SRP/4.GpuInstance/GpuInstanceMatrilBlock/2GpuInstanceTest/GpuInstancingTest.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class GpuInstancingTest : MonoBehaviour
+{
+    public GameObject preObj;
+    public Mesh mesh;
+    Material material;
+
+    List<Matrix4x4> matrices = new List<Matrix4x4>();
+
+    void Start()
+    {
+        material = preObj.GetComponent<MeshRenderer>().sharedMaterial;
+        material.enableInstancing = true;
+        if (!material.enableInstancing)
+        {
+            Debug.LogError("无法开启 !material.enableInstancing");
+            enabled = false;
+            return;
+        }
+        if (!SystemInfo.supportsInstancing)
+        {
+            Debug.LogError("无法开启 !SystemInfo.supportsInstancing");
+            enabled = false;
+            return;
+        }
+        block = new MaterialPropertyBlock();
+        mesh = preObj.GetComponent<MeshFilter>().mesh;
+
+    }
+    
+    MaterialPropertyBlock block = null;
+    void OnPostRender() //camera
+    {
+        // SetPass to 0 if the material doesnt have a texture.
+        //如果材质没有个纹理，SetPass为0
+
+        //for (int i = 0; i < 20; ++i)
+        //{
+        //    float r = Random.Range(0, 1f);
+        //    float g = Random.Range(0, 1f);
+        //    float b = Random.Range(0, 1f);
+        //    Matrix4x4 mt = Matrix4x4.TRS(transform.position + (i * 1.1f) * Vector3.up, transform.rotation, Vector3.one);
+        //    matrices.Add(mt);
+
+        //    preObj.GetComponent<MeshRenderer>().GetPropertyBlock(block);
+        //    block.SetColor("_Color", new Color(r, g, b, 1));
+        //     block.SetTexture("_MainTex", null);
+        //    preObj.GetComponent<MeshRenderer>().SetPropertyBlock(block);
+
+        //    Graphics.DrawMeshNow(mesh, new Vector3(i, i, i), Quaternion.identity);
+        //}
+    }
+
+    // 两者都需要在for循环内部，每次单独提交
+    // 但 drawmeshNow 用于OnPostRender
+    // DrawMeshInstanced 用于Update中
+    void Update()
+    {
+        MeshRenderer mMeshRenderer = preObj.GetComponent<MeshRenderer>();
+        for (int i = 0; i < 100; ++i)
+        {
+            //for (int j = 0; j < 100; ++j)
+            {
+                Vector3 pos = preObj.transform.position + new Vector3(i * 1.2f, i * 1.2f, i * 1.2f);
+
+                Matrix4x4 mt = Matrix4x4.TRS(pos, preObj.transform.rotation, Vector3.one);
+                matrices.Add(mt);
+
+                //  GameObject go = Instantiate(prefab);
+                //go.transform.Translate(new Vector3(Mathf.Cos(i), Mathf.Sin(i), 0) * i/10f);
+
+                float r = Random.Range(0, 1f);
+                float g = Random.Range(0, 1f);
+                float b = Random.Range(0, 1f);
+
+                mMeshRenderer.GetPropertyBlock(block);
+                block.SetColor("_OutsideColor", new Color(i / 50.0f, i / 10.0f, i / 50.0f, 1));
+                //block.SetFloat("_Sin", i);
+                //当每个对象的材料只有几个属性不同时，建议使用此方法。与每个对象具有一个完全不同的材质相比，这具有更高的内存效率。
+                mMeshRenderer.SetPropertyBlock(block);
+
+
+                // public static void DrawMesh(Mesh mesh, Vector3 position, Quaternion rotation, Material material, 
+                //int layer, Camera camera, int submeshIndex, MaterialPropertyBlock properties);
+                // Graphics.DrawMeshInstanced(mesh, 0, material, matrices.ToArray(), 1, block);
+               Graphics.DrawMesh(mesh, pos, Quaternion.identity, material, 2 << 1, Camera.main, 0, block); ;
+
+            }
+        }
+
+    }
+}
---
+++ Unity6_SRP/Assets/SRP/4.GpuInstance/GpuInstanceMatrilBlock/2GpuInstanceTest/testInstance.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class testInstance : MonoBehaviour
+{
+    //材质用到的mesh
+    Mesh mesh;
+    Material mat;
+    public GameObject m_prefab;
+    Matrix4x4[] matrix;
+    ShadowCastingMode castShadows;//阴影选项
+    public int InstanceCount = 10;
+    //树的预制体由树干和树叶两个mesh组成
+    MeshFilter[] meshFs;
+    Renderer[] renders;
+
+    //这个变量类似于unity5.6材质属性的Enable Instance Variants勾选项
+    public bool turnOnInstance = true;
+    void Start()
+    {
+        if (m_prefab == null)
+            return;
+        Shader.EnableKeyword("LIGHTMAP_ON");//开启lightmap
+                                            //Shader.DisableKeyword("LIGHTMAP_OFF");
+        var mf = m_prefab.GetComponent<MeshFilter>();
+        if (mf)
+        {
+            mesh = m_prefab.GetComponent<MeshFilter>().sharedMesh;
+            mat = m_prefab.GetComponent<Renderer>().sharedMaterial;
+        }
+        //如果一个预制体 由多个mesh组成，则需要绘制多少次
+        if (mesh == null)
+        {
+            meshFs = m_prefab.GetComponentsInChildren<MeshFilter>();
+        }
+        if (mat == null)
+        {
+            renders = m_prefab.GetComponentsInChildren<Renderer>();
+        }
+        matrix = new Matrix4x4[InstanceCount];
+
+        castShadows = ShadowCastingMode.On;
+
+        //随机生成位置与缩放
+        for (int i = 0; i < InstanceCount; i++)
+        {
+            ///   random position
+            float x = Random.Range(-50, 50);
+            float y = Random.Range(-3, 3);
+            float z = Random.Range(-50, 50);
+            matrix[i] = Matrix4x4.identity;   ///   set default identity
+            //设置位置
+            matrix[i].SetColumn(3, new Vector4(x, 0.5f, z, 1));  /// 4th colummn: set   position
+            //设置缩放
+            //matrix[i].m00   = Mathf.Max(1, x);
+            //matrix[i].m11   = Mathf.Max(1, y);
+            //matrix[i].m22   = Mathf.Max(1, z);
+        }
+    }
+
+    MaterialPropertyBlock props = null;
+
+    void Update()
+
+    //.通过Matrix4x4.TRS(position, rotation, scale)；把所有草的坐标算出来，存入数组中。
+    //3.通过MaterialPropertyBlock给shader传递需要修改的值，一般用SetVectorArray或者SetFloatArray，因为我们希望这一批草或其它什么有不同的属性。
+    //  Graphics.DrawMeshInstanced(Mesh, submeshIndex, Material, matrices, count, properties);
+    /*
+        Mesh 索要绘制的网格
+        submeshIndex 要绘制网格的哪个子集,由多个网格组成的才用得到，一般为0
+        Material 材质
+        matrices 在步骤2中计算的矩阵数组
+        count 数量 最多只能绘制1023个实例，也就是说count<=1023。
+        properties 步骤3中创建的MaterialPropertyBlock，用来传递属性
+        还有很多参数，不过画草的话用不到	
+    */
+
+    {//Graphics.DrawMeshInstanced() 在update函数实例化
+        if (turnOnInstance)
+        {
+            //props = obj.GetComponent<MeshRenderer>().GetPropertyBlock(block);
+            castShadows = ShadowCastingMode.On;
+            if (mesh)
+                Graphics.DrawMeshInstanced(mesh, 0, mat, matrix, matrix.Length, props, castShadows, true, 0, null);
+            else
+            {
+                for (int i = 0; i < meshFs.Length; ++i)
+                {
+                    Graphics.DrawMeshInstanced(meshFs[i].sharedMesh, 0, renders[i].sharedMaterial, matrix, matrix.Length, props, castShadows, true, 0, null);
+                }
+            }
+        }
+    }
+}
